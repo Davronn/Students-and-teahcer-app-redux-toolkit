c161ea12f0fc6c76b0f289d5eb7185541b2d0700
Davronn <davronmahamadjonov8@gmai.com>
2024-04-19 15:30:03 +0500

    .

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,32 +1,48 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
 function Modall(props) {
   return (
     <Modal
       {...props}
-      size="lg"
+      size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
+      <Modal.Header>
+        <h1>Student Add</h1>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <div>
+          <Form>
+            <div className="m-5">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="text" />
+              </Form.Group>
+              <Form.Select aria-label="Default select example">
+                <option>Select</option>
+                <option value="N45">N45</option>
+                <option value="N32">N32</option>
+                <option value="N29">N29</option>
+              </Form.Select>
+            </div>
+            <div className="m-5">
+              <Button className="mx-2" variant="primary" type="submit">
+                Submit
+              </Button>
+              <Button onClick={props.onHide}>Close</Button>
+            </div>
+          </Form>
+        </div>
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
     </Modal>
   );
 }
 
-export default Modall
+export default Modall;
